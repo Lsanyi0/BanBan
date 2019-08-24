501c646b5fba10d22c8ce04ae3c040e8bf92f216
Lsanyi0 <sanyi0@hotmail.es>
2019-08-23 23:05:34 -0600

    Se agrego el boton de opciones

--- BanBan/Properties/Resources.Designer.cs
+++ BanBan/Properties/Resources.Designer.cs
@@ -59,5 +59,15 @@ namespace BanBan.Properties {
                 resourceCulture = value;
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap menu_button {
+            get {
+                object obj = ResourceManager.GetObject("menu_button", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
